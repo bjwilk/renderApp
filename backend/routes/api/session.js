2b6a088221878b05d8e10921e38198faf3b082ed
Brian Wilkinson <brian.joseph.wilkinson@gmail.com>
2024-02-10 17:33:02 -0800

    added user input validation

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -6,7 +6,23 @@ const bcrypt = require('bcryptjs');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+
+const validateLogin = [
+    check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+    check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+    handleValidationErrors
+];
+
 const router = express.Router();
+
 
 // Restore session user
 router.get(
@@ -30,6 +46,7 @@ router.get(
 // Log in
 router.post(
     '/',
+    validateLogin,
     async (req, res, next) => {
       const { credential, password } = req.body;
   
@@ -63,7 +80,6 @@ router.post(
       });
     }
   );
-
 
   // Log out
 router.delete(
