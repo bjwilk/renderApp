97967701fb00bd7dfad6a424f0aa83329f6dd86e
Brian Wilkinson <brian.joseph.wilkinson@gmail.com>
2024-03-02 11:33:43 -0800

    updated down

--- backend/db/seeders/20240215021721-demo-review-image.js
+++ backend/db/seeders/20240215021721-demo-review-image.js
@@ -26,6 +26,7 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
+    options.tableName = "ReviewImages"
     return queryInterface.bulkDelete(options, 'ReviewImages', null, {});
 
   }
