a05cc17abd7a3ef9b65956e136859c30731ceb4b
Brian Wilkinson <brian.joseph.wilkinson@gmail.com>
2024-06-03 20:49:39 -0700

    edit

--- frontend/src/components/HomePage/HomePage.jsx
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -7,7 +7,6 @@ const HomePage = () => {
   const dispatch = useDispatch();
   const spots = useSelector((state) => state.spots);
 
-  console.log(spots);
 
   useEffect(() => {
     dispatch(fetchSpots());
