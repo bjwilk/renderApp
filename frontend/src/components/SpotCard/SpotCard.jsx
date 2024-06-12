0bb93d47a41121a4c8bcca882c96444384e30bc2
Brian Wilkinson <brian.joseph.wilkinson@gmail.com>
2024-06-11 21:14:21 -0700

    updated home images

--- frontend/src/components/SpotCard/SpotCard.jsx
+++ frontend/src/components/SpotCard/SpotCard.jsx
@@ -2,11 +2,18 @@ import "./SpotCard.css";
 import { NavLink } from "react-router-dom";
 
 function SpotCard({ spot }) {
+  const previewImage = spot.previewImage;
   return (
     <div>
       <NavLink to={`/spots/${spot.id}`}>
         <h3>{spot.name}</h3>
-        <div>{spot.previewImage}</div>
+        <div className="card-preview-image">
+          {previewImage ? (
+            <img src={previewImage} alt={spot.name} />
+          ) : (
+            "No image"
+          )}
+        </div>
         <p>{spot.price}</p>
         <p>{spot.description}</p>
       </NavLink>
