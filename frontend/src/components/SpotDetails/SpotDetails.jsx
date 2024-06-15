3de5baf4c4472fc23207d0ae6c860e75f694e8a9
Brian Wilkinson <brian.joseph.wilkinson@gmail.com>
2024-06-14 18:14:59 -0700

    redo

--- frontend/src/components/SpotDetails/SpotDetails.jsx
+++ frontend/src/components/SpotDetails/SpotDetails.jsx
@@ -8,7 +8,6 @@ import CreateReview from "../CreateReview/CreateReview";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar as regularStar } from "@fortawesome/free-regular-svg-icons";
 import { faStar as solidStar } from "@fortawesome/free-solid-svg-icons";
-import { faStar } from "@fortawesome/free-solid-svg-icons";
 import { useModal } from "../../context/Modal";
 import ConfirmDeleteModal from "../ConfirmDeleteModal/ConfirmDeleteModal";
 
@@ -29,14 +28,9 @@ function SpotDetails() {
   const [error, setError] = useState(null);
   const [selectedReviewId, setSelectedReviewId] = useState(null);
 
-  const spotReviews = [];
-
-  const reviewList = Object.values(reviews);
-  Object.values(reviewList).map((item) => {
-    if (item.spotId == spotId) {
-      spotReviews.push(item);
-    }
-  });
+  const spotReviews = Object.values(reviews).filter(
+    (review) => review.spotId == spotId
+  );
 
   useEffect(() => {
     dispatch(fetchSpotDetails(spotId))
@@ -55,34 +49,19 @@ function SpotDetails() {
   const spotOwner = user && user.id === spot.ownerId;
 
   const spotImages = Object.values(spot.SpotImages);
+  const previewImage = spotImages.find((image) => image.preview)?.url;
+  const otherImages = spotImages.filter((image) => !image.preview).map(image => image.url);
 
-  const previewImage = spotImages.map((index) => {
-    return index.url;
-  });
-
-  let hasReview;
-  if (user && spotReviews.find((review) => review.User.id == user.id)) {
-    hasReview = true;
-  }
-
-  const otherImages = [];
-
-  spotImages.map((image) => {
-    if (image.preview !== true) {
-      otherImages.push(image.url);
-    }
-  });
+  const hasReview = user && spotReviews.some((review) => review.User.id == user.id);
 
   const starIcons = [];
   const fullStars = Math.floor(spot.avgStarRating);
   const hasHalfStar = spot.avgStarRating % 1 !== 0;
 
-  // Push full stars
   for (let i = 0; i < fullStars; i++) {
     starIcons.push(<FontAwesomeIcon key={i} icon={solidStar} />);
   }
 
-  // Push half star if needed
   if (hasHalfStar) {
     starIcons.push(
       <FontAwesomeIcon key="half" icon={regularStar} className="half-star" />
@@ -94,11 +73,9 @@ function SpotDetails() {
     setShowModal(true);
   };
 
-
   const handleConfirmDelete = () => {
     dispatch(fetchRemoveReview(selectedReviewId))
       .then(() => {
-        dispatch(fetchRemoveReview(selectedReviewId))
         setShowModal(false);
         setSelectedReviewId(null);
       })
@@ -124,7 +101,7 @@ function SpotDetails() {
         <div className="images-container">
           <div className="preview-image">
             {previewImage ? (
-              <img src={previewImage.toString()} alt={spot.name} />
+              <img src={previewImage} alt={spot.name} />
             ) : (
               "No image"
             )}
@@ -142,7 +119,7 @@ function SpotDetails() {
           </div>
         </div>
       </div>
-      <br></br>
+      <br />
       <div className="details-section">
         <div className="spot-info">
           <h2>
@@ -152,10 +129,10 @@ function SpotDetails() {
         </div>
         <div className="spot-info-box">
           ${spot.price} night
-          <br></br>
+          <br />
           <div>{starIcons}</div>
           {spot.avgStarRating.toFixed(2)}
-          <br></br>
+          <br />
           {spot.numReviews ? (
             "Reviews"
           ) : (
@@ -168,18 +145,7 @@ function SpotDetails() {
       </div>
 
       <div>
-        <h4>
-          {spot.numReviews ? (
-            "Reviews"
-          ) : (
-            <>
-              <FontAwesomeIcon icon={regularStar} /> New
-            </>
-          )}
-          <br></br>
-          {spot.avgStarRating.toFixed(2)}
-          <div>{starIcons}</div>
-        </h4>
+        <h4>Reviews</h4>
 
         {!spotOwner && user && !hasReview && (
           <div>
@@ -189,35 +155,32 @@ function SpotDetails() {
             />
           </div>
         )}
-        {spotReviews && Object.keys(spotReviews).length > 0 ? (
-          Object.values(spotReviews)
-            .reverse()
-            .map((review) => (
-              <div key={review.id}>
-                <h3>{review.User?.firstName || "Anonymous"}</h3>
-                <span>{new Date(review.createdAt).toLocaleDateString()}</span>
-                <p>{review.review}</p>
-                {review.userId == user.id && (
-                  <>
-                    <button onClick={() => handleDeleteModal(review.id)}>Delete</button>
-                  </>
-                )}
-              </div>
-            ))
+        {spotReviews && spotReviews.length > 0 ? (
+          spotReviews.reverse().map((review) => (
+            <div key={review.id}>
+              <h3>{review.User?.firstName || "Anonymous"}</h3>
+              <span>{new Date(review.createdAt).toLocaleDateString()}</span>
+              <p>{review.review}</p>
+              {user && review.userId === user.id && (
+                <>
+                  <button onClick={() => handleDeleteModal(review.id)}>Delete</button>
+                </>
+              )}
+            </div>
+          ))
         ) : (
           <p>No reviews yet. Be the first to leave a review!</p>
         )}
       </div>
 
       {showModal && (
-        <div>
-          <ConfirmDeleteModal
-            onConfirm={handleConfirmDelete}
-            onCancel={handleCancelDelete}
-          />
-        </div>
+        <ConfirmDeleteModal
+          onConfirm={handleConfirmDelete}
+          onCancel={handleCancelDelete}
+        />
       )}
     </>
   );
 }
+
 export default SpotDetails;
